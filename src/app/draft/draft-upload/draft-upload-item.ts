9f2ae73ba198e765fed5af2af04a6473a2860be5
fabioyk <kyongxd@gmail.com>
2017-06-05 17:00:16 -0300

    prod api

--- src/app/draft/draft-upload/draft-upload-item.ts
+++ src/app/draft/draft-upload/draft-upload-item.ts
@@ -3,7 +3,7 @@ import { UploadItem }    from 'angular2-http-file-upload';
 export class DraftUploadItem extends UploadItem {
     constructor(file: any, isAnonymous:boolean) {
         super();        
-        this.url = 'https://crack-a-draft.glitch.me/api/draft?date=' + file.lastModified;
+        this.url = 'https://crack-a-draft.herokuapp.com/api/draft?date=' + file.lastModified;
         if (isAnonymous) {
             this.url += '&anonymous=true';
         }        
