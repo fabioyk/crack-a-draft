ff6ca7951d53d0c99a7b53399798690af0d77f65
fabioyk <kyongxd@gmail.com>
2017-06-05 16:36:11 -0300

    pass the modified date to the api to be used later

--- src/app/draft/draft-upload/draft-upload-item.ts
+++ src/app/draft/draft-upload/draft-upload-item.ts
@@ -3,12 +3,11 @@ import { UploadItem }    from 'angular2-http-file-upload';
 export class DraftUploadItem extends UploadItem {
     constructor(file: any, isAnonymous:boolean) {
         super();        
-        this.url = 'https://crack-a-draft.herokuapp.com/api/draft';
+        this.url = 'https://crack-a-draft.glitch.me/api/draft?date=' + file.lastModified;
         if (isAnonymous) {
-            this.url += '?anonymous=true';
-        }
+            this.url += '&anonymous=true';
+        }        
 
-        //this.headers = { Content-Type: 'multipart/form-data'};
         this.file = file;
     }
 }
