8acde0d58e2cc03f9ae0aad7c54a51331e93cf53
fabioyk <kyongxd@gmail.com>
2017-09-25 17:29:22 -0300

    fixed change to glitch

--- src/app/draft/draft-upload/draft-upload-item.ts
+++ src/app/draft/draft-upload/draft-upload-item.ts
@@ -6,7 +6,8 @@ export class DraftUploadItem extends UploadItem {
     constructor(file: any, isAnonymous:boolean) {
         super();  
         if (environment.production) {
-            this.url = 'https://crack-a-draft.herokuapp.com/api/';
+            //this.url = 'https://crack-a-draft.herokuapp.com/api/';
+            this.url = 'https://crack-a-draft.glitch.me/api/'            
         } else {
             this.url = 'https://crack-a-draft.glitch.me/api/'
         }      
